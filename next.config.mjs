54220c20582d919bf9ebb709e0262845da4f919f
Juanma <juangpdev@gmail.com>
2024-11-20 12:52:51 +0100

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
         locales: ['en-US', 'es-ES'],
         defaultLocale: 'es-ES',
     },
+    reactStrictMode: true,
+
+    // Configurar compilación
+    swcMinify: true,
+
+    // Configurar salida estática si es necesario
+    output: 'standalone',
+
+    // Configurar dominio de imágenes si se usan
+    images: {
+        domains: ['localhost'],
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
